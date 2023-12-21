0fa35e1fb78480d1d9361ee1dc965eb228b29c63
webnur <mdnuruli291@gmail.com>
2023-12-21 23:28:39 +0600

    [done] get all faculty

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { facultyFilterableFields } from './faculty.constants';
 import { FacultyService } from './faculty.service';
 
 const createFaculty = catchAsync(async (req: Request, res: Response) => {
@@ -15,12 +17,15 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFaculty = catchAsync(async (req: Request, res: Response) => {
-  const result = await FacultyService.getAllFaculty();
+  const filters = pick(req.query, facultyFilterableFields);
+  const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder']);
+  const result = await FacultyService.getAllFaculty(filters, options);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'get all faculty successfully!',
-    data: result,
+    meta: result.meta,
+    data: result.data,
   });
 });
 
--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -1,14 +1,78 @@
-import { Faculty } from '@prisma/client';
+import { Faculty, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import { IStudentFilterRequest } from '../student/student.interface';
+import {
+  facultyRelationalFields,
+  facultyRelationalFieldsMapper,
+  facultySearchableFields,
+} from './faculty.constants';
 
 const createFaculty = async (data: Faculty): Promise<Faculty> => {
   const result = await prisma.faculty.create({ data });
   return result;
 };
 
-const getAllFaculty = async (): Promise<Faculty[]> => {
+const getAllFaculty = async (
+  filters: IStudentFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Faculty[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+  if (searchTerm) {
+    andConditions.push({
+      OR: facultySearchableFields.map(field => ({
+        [field]: { contains: searchTerm, mode: 'insensitive' },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (facultyRelationalFields.includes(key)) {
+          return {
+            [facultyRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+  const whereConditions: Prisma.FacultyWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
   const result = await prisma.faculty.findMany();
-  return result;
+
+  const total = await prisma.faculty.count({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
 };
 
 const getSingleFaculty = async (id: string): Promise<Faculty | null> => {
